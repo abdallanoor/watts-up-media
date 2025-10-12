df0d4218b04582446b292bb407c9d3d9174af986
Abdallah <abdallaahnoor@gmail.com>
2025-10-12 19:50:19 +0300

    refactor theme-toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -23,11 +23,11 @@ export function ThemeToggle() {
       className="w-8 h-8 rounded-full cursor-pointer hover:bg-transparent!"
     >
       <Sun
-        className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-muted-foreground"
+        className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-muted-foreground"
         fill="currentColor"
       />
       <Moon
-        className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-muted-foreground"
+        className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-muted-foreground"
         fill="currentColor"
       />
       <span className="sr-only">Toggle theme</span>
